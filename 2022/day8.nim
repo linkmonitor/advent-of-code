5d2a3256bba85ef3a75be87ec06787c9ab0e8e07
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-09 23:24:20 -0800

    2022 Day 8 Part 1

--- 2022/day8.nim
+++ 2022/day8.nim
@@ -1,49 +1,31 @@
 import arraymancer
-import std/[sequtils, strutils, sugar]
+import std/[sequtils, strutils]
+import util
 
 const kInput = staticRead("day8.txt").strip.splitLines
 const (kRows, kCols) = (kInput.len, kInput[0].len)
 const kNumbers = kInput.join.mapIt(parseInt($it))
 let kTensor = kNumbers.toTensor.reshape(kRows, kCols)
 
-proc scan[T, U](arr:openarray[T], start:U, f:(T, U) -> U): seq[U] =
-  var last = start
-  for elem in arr:
-    last = f(last, elem)
-    result.add(last)
-
-proc max[T](s1, s2: Tensor[T]):Tensor[T] =
-  result = clone(s1)
-  doAssert(s1.shape == s2.shape)
-  for (idx, item )in result.menumerate:
-    item = max(s1.atContiguousIndex(idx),
-               s2.atContiguousIndex(idx))
-
-proc increasing[T](s1, s2:Tensor[T]):Tensor[T] =
-  result = clone(s1)
-  doAssert(s1.shape == s2.shape)
-  for (idx, item) in result.menumerate:
-    item[0] = s2.atContiguousIndex(idx)[0]
-    item[1] = int(s2.atContiguousIndex(idx)[0] >
-                  s1.atContiguousIndex(idx)[0])
-
-proc visibleFromLeft[T](t:Tensor[T]):Tensor[int] =
-  let shape = t.shape
-  let zero_col = zeros[T](shape[0], 1)
-  let max_scan = t.split(1, 1).scan(zero_col, max).concat(1)
-  let neg1_col = (ones[int](shape[0],1) * -1).map(x => (x, 0))
-  let increase = max_scan.map(x => (x, 0)).split(1, 1).scan(neg1_col, increasing).concat(1)
-  increase.map(x => int(x[1] > 0))
+proc visibleFromTop[T](t:Tensor[T]):Tensor[int] =
+  func umax(x,y:int):int = max(x,y)
+  let max_scan = t.ScanRows(umax, zerosLike[T](t[0, _]))
+  func ugrt_s(x:tuple[a, b:int], y:int):type(x) = (max(x.a, y), int(y > x.a))
+  func augment(x:int):tuple[a,b:int] = (x, 0)
+  func extract(x:tuple[a, b:int]):int = x.b
+  let neg_one = onesLike[int](t[0,_]).negate
+  let increase = max_scan.ScanRows(ugrt_s, neg_one.map(augment))
+  increase.map(extract).clamp(0, 1)
 
 proc visible[T](t:Tensor[T]):Tensor[int] =
-  let left = t.visibleFromLeft
-  let right = t[_, ^1..0|-1].visibleFromLeft[_, ^1..0|-1]
-  let top = t.transpose.visibleFromLeft.transpose
-  let bott = t[^1..0|-1, _].transpose.visibleFromLeft.transpose[^1..0|-1, _]
-  left +. right +. top +. bott
+  let top = t.visibleFromTop
+  let bot = t.Flipped.visibleFromTop.Flipped
+  let lef = t.transpose.visibleFromTop.transpose
+  let rig = t.Reversed.transpose.visibleFromTop.transpose.Reversed
+  top +. bot +. lef +. rig
 
 proc main =
-  echo(kTensor.visible.toSeq.countIt(it > 0))
+  echo("Part1: ", kTensor.visible.clamp(0, 1).sum)
 
 when isMainModule:
   main()
--- 2022/util.nim
+++ 2022/util.nim
@@ -1,3 +1,6 @@
+import arraymancer
+import std/[sugar]
+
 template doTimes*(n:Natural, body:typed) =
   for _ in 0..<n:
     body
@@ -9,3 +12,33 @@ proc IndexFirst*[T](arr:openArray[T], pred:proc(x:T):bool):int =
 
 proc ElementFirst*[T](arr:openArray[T], pred:proc(x:T):bool):T =
   arr[arr.IndexFirst(pred)]
+
+template universal[T, U, V](f:(T, U) -> V):auto =
+  const u = proc(a:Tensor[T], b:Tensor[U]):Tensor[V] =
+    doAssert(a.shape == b.shape)
+    result = clone(a)
+    for (idx, elt) in result.menumerate:
+      elt = f(a.atContiguousIndex(idx), b.atContiguousIndex(idx))
+  u
+
+proc Scan*[T, U](xs:openarray[T], x0:U, f:(U, T) -> U): seq[U] =
+  var xn_1 = x0
+  for x in xs:
+    xn_1 = f(xn_1, x)
+    result.add(xn_1)
+
+proc ScanRows*[T, U](t:Tensor[T], f:proc(x:U,y:T):U, t0:Tensor[U]): Tensor[U] =
+  const uf = universal(f)
+  t.split(1,0).Scan(t0, uf).concat(0)
+
+proc ScanRows*[T, U](t:Tensor[T], f:proc(x:U,y:T):U): Tensor[U] =
+  t[1.._, _].ScanRows(f, t[0,_])
+
+proc ScanCols*[T, U](t:Tensor[T], f:proc(x:U,y:T):U): Tensor[U] =
+  t.transpose.ScanRows(f).transpose
+
+proc ScanCols*[T, U](t:Tensor[T], f:proc(x:U,y:T):U, t0:Tensor[U]): Tensor[U] =
+  t.transpose.ScanRows(f, t0.transpose).transpose
+
+proc Flipped*[T](t:Tensor[T]):Tensor[T] = t[^1..0|-1, _]
+proc Reversed*[T](t:Tensor[T]):Tensor[T] = t[_, ^1..0|-1]
