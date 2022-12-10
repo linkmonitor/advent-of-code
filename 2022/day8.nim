dee6bd64dac9649fa0f1e910db50d20e944a7fe3
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-10 13:43:46 -0800

    Refactor 2022 Day 8 Part 2

--- 2022/day8.nim
+++ 2022/day8.nim
@@ -20,35 +20,22 @@ proc visible[T](t:Tensor[T]):Tensor[int] =
   let rig = t.Reversed.transpose.visibleFromTop.transpose.Reversed
   (top +. bot +. lef +. rig).clamp(0, 1)
 
+proc treesVisible1d[T](neighbors:Tensor[T], limit:int):int =
+  for neighbor in neighbors:
+    result.inc
+    if neighbor >= limit: break
+
 proc scenicScore[T](t:Tensor[T], tree:tuple[a,b:int]):int =
+  # All trees on the edge have a scenic score of zero
   let (kRows, kCols) = (t.shape[0], t.shape[1])
+  if tree.a == 0 or tree.b == 0 or (tree.a == kRows-1) or (tree.b == kCols-1):
+    return 0
   let height = t[tree.a, tree.b]
   result = 1
-  var scores:seq[int]
-  var trees = 0
-  for col in (tree.b+1)..<kCols:
-    trees.inc
-    if t[tree.a, col] >= height: break
-  scores.add(trees)
-  result *= trees
-  trees = 0
-  for col in countdown(tree.b-1, 0):
-    trees.inc
-    if t[tree.a, col] >= height: break
-  scores.add(trees)
-  result *= trees
-  trees = 0
-  for row in (tree.a+1)..<kRows:
-    trees.inc
-    if t[row, tree.b] >= height: break
-  scores.add(trees)
-  result *= trees
-  trees = 0
-  for row in countdown(tree.a-1, 0):
-    trees.inc
-    if t[row, tree.b] >= height: break
-  scores.add(trees)
-  result *= trees
+  result *= t[tree.a+1.._, tree.b].treesVisible1d(height)
+  result *= t[tree.a-1..0|-1, tree.b].treesVisible1d(height)
+  result *= t[tree.a, tree.b+1.._].treesVisible1d(height)
+  result *= t[tree.a, tree.b-1..0|-1].treesVisible1d(height)
 
 proc highestScenicScore[T](t:Tensor[T]):int =
   for (coord, _) in t.pairs:
