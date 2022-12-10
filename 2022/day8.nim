5391f4da7e12b3a581087fcf64bf2df8f56104a1
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-10 12:24:02 -0800

    Refactor 2022 Day 8 Part 1

--- 2022/day8.nim
+++ 2022/day8.nim
@@ -1,31 +1,25 @@
 import arraymancer
-import std/[sequtils, strutils]
+import std/[sequtils, strutils, sugar]
 import util
 
 const kInput = staticRead("day8.txt").strip.splitLines
 const (kRows, kCols) = (kInput.len, kInput[0].len)
-const kNumbers = kInput.join.mapIt(parseInt($it))
-let kTensor = kNumbers.toTensor.reshape(kRows, kCols)
+let kTensor = kInput.join.mapIt(parseInt($it)).toTensor.reshape(kRows, kCols)
 
 proc visibleFromTop[T](t:Tensor[T]):Tensor[int] =
-  func umax(x,y:int):int = max(x,y)
-  let max_scan = t.ScanRows(umax, zerosLike[T](t[0, _]))
-  func ugrt_s(x:tuple[a, b:int], y:int):type(x) = (max(x.a, y), int(y > x.a))
-  func augment(x:int):tuple[a,b:int] = (x, 0)
-  func extract(x:tuple[a, b:int]):int = x.b
-  let neg_one = onesLike[int](t[0,_]).negate
-  let increase = max_scan.ScanRows(ugrt_s, neg_one.map(augment))
-  increase.map(extract).clamp(0, 1)
+  let neg_one = onesLike[int](t[0, _]).negate.map(x => (x, 0))
+  func smax_ainc(x:tuple[a, b:int], y:int):type(x) = (max(x.a, y), int(y > x.a))
+  t.ScanRows(smax_ainc, neg_one).map(x => x.b)
 
 proc visible[T](t:Tensor[T]):Tensor[int] =
   let top = t.visibleFromTop
   let bot = t.Flipped.visibleFromTop.Flipped
   let lef = t.transpose.visibleFromTop.transpose
   let rig = t.Reversed.transpose.visibleFromTop.transpose.Reversed
-  top +. bot +. lef +. rig
+  (top +. bot +. lef +. rig).clamp(0, 1)
 
 proc main =
-  echo("Part1: ", kTensor.visible.clamp(0, 1).sum)
+  echo("Part1: ", kTensor.visible.sum)
 
 when isMainModule:
   main()
