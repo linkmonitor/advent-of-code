efd08fa87a38236ef2f4c8df690ade818288f5e3
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-11 13:05:15 -0800

    Refactor 2022 Day 9 Part 1

--- 2022/day9.nim
+++ 2022/day9.nim
@@ -1,4 +1,5 @@
-import std/[sets, sequtils, strscans, strutils, sugar]
+import std/[sets, sequtils, strscans, strutils]
+import util
 
 type
   Delta    = array[2, int]
@@ -7,33 +8,24 @@ type
 
 func ParseDelta(input:string):Delta =
   let (_, dir, dist) = input.scanTuple("$+ $i")
-  if dir == "U": return [0, dist]
-  if dir == "D": return [0, -dist]
-  if dir == "L": return [-dist, 0]
-  if dir == "R": return [dist, 0]
-
-func ToDelta(a, b:Position):Delta = [a[0] - b[0], a[1] - b[1]]
-func Clamp(p:Position, a, b:int):Position = [p[0].clamp(a,b), p[1].clamp(a,b)]
-
-proc Apply(position:var Position, delta:Delta) =
-  position[0] += delta[0]
-  position[1] += delta[1]
+  if dir == "U": return [    0,  dist]
+  if dir == "D": return [    0, -dist]
+  if dir == "L": return [-dist,     0]
+  if dir == "R": return [ dist,     0]
 
 proc Apply(context:var Context, delta:Delta) =
-  let times = abs(delta[0] + delta[1])
+  let times = delta.Abs.max
   let delta = delta.Clamp(-1, 1)
   context.tails.incl(context.tail)
   for _ in 0..<times:
-    context.head.Apply(delta)
-    let diff = ToDelta(context.head, context.tail)
-    if diff[0] in [-2, 2] or diff[1] in [-2,2]:
-      let diff = diff.Clamp(-1, 1)
-      context.tail.Apply(diff)
+    context.head += delta
+    let diff = context.head - context.tail
+    if 2 in diff.Abs:
+      context.tail += diff.Clamp(-1, 1)
       context.tails.incl(context.tail)
 
-const deltas = staticRead("day9.txt").strip.splitLines.map(ParseDelta)
-
 proc main =
+  const deltas = staticRead("day9.txt").strip.splitLines.map(ParseDelta)
   var context:Context
   for delta in deltas: context.Apply(delta)
   echo("Part1: ", context.tails.len)
--- 2022/util.nim
+++ 2022/util.nim
@@ -29,15 +29,25 @@ proc Scan*[T, U](xs:openarray[T], x0:U, f:(U, T) -> U): seq[U] =
 proc ScanRows*[T, U](t:Tensor[T], f:proc(x:U,y:T):U, t0:Tensor[U]): Tensor[U] =
   const uf = universal(f)
   t.split(1,0).Scan(t0, uf).concat(0)
-
 proc ScanRows*[T, U](t:Tensor[T], f:proc(x:U,y:T):U): Tensor[U] =
   t[1.._, _].ScanRows(f, t[0,_])
-
 proc ScanCols*[T, U](t:Tensor[T], f:proc(x:U,y:T):U): Tensor[U] =
   t.transpose.ScanRows(f).transpose
-
 proc ScanCols*[T, U](t:Tensor[T], f:proc(x:U,y:T):U, t0:Tensor[U]): Tensor[U] =
   t.transpose.ScanRows(f, t0.transpose).transpose
 
 proc Flipped*[T](t:Tensor[T]):Tensor[T] = t[^1..0|-1, _]
 proc Reversed*[T](t:Tensor[T]):Tensor[T] = t[_, ^1..0|-1]
+
+func `-`*[M, T](a, b:array[M, T]):array[M, T] =
+  for idx, value in result.mpairs: value = a[idx] - b[idx]
+func `+`*[M, T](a, b:array[M, T]):array[M, T] =
+  for idx, value in result.mpairs: value = a[idx] + b[idx]
+func Clamp*[M, T](a:array[M,T], n, m:int):array[M, T] =
+  for idx, value in result.mpairs: value = a[idx].clamp(n, m)
+func Abs*[M, T](a:array[M,T]):array[M, T] =
+  for idx, value in result.mpairs: value = abs(a[idx])
+proc  `+=`*[M, T](a:var array[M, T], b:array[M, T]) =
+  for idx, value in a.mpairs: value += b[idx]
+
+
