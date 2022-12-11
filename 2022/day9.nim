8f8d5227981c43accec5cc1884eccc13a693e615
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-11 13:22:43 -0800

    Refactor 2022 Day 9 Part 1

--- 2022/day9.nim
+++ 2022/day9.nim
@@ -4,7 +4,7 @@ import util
 type
   Delta    = array[2, int]
   Position = array[2, int]
-  Context  = tuple[head, tail:Position, tails:HashSet[Position]]
+  Context  = tuple[head, tail:Position, tailpos:HashSet[Position]]
 
 func ParseDelta(input:string):Delta =
   let (_, dir, dist) = input.scanTuple("$+ $i")
@@ -18,20 +18,20 @@ proc Chase(tail:var Position, head: Position) =
   if 2 in diff.Abs: tail += diff.Clamp(-1, 1)
 
 proc Apply(context:var Context, delta:Delta) =
-  context.tails.incl(context.tail)
+  context.tailpos.incl(context.tail)
   # Move one space at a time to capture all the tail locations.
   let times = delta.Abs.max
   let delta = delta.Clamp(-1, 1)
   doTimes times:
     context.head += delta
     context.tail.Chase(context.head)
-    context.tails.incl(context.tail)
+    context.tailpos.incl(context.tail)
 
 proc main =
   const deltas = staticRead("day9.txt").strip.splitLines.map(ParseDelta)
   var context:Context
   for delta in deltas: context.Apply(delta)
-  echo("Part1: ", context.tails.len)
+  echo("Part1: ", context.tailpos.len)
 
 when isMainModule:
   main()
