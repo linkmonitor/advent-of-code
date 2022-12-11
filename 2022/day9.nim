b9b875e4a204d00a5d6d16bf6260d2197b131d3d
Joseph Jaoudi <linkmonitor@gmail.com>
2022-12-11 13:33:45 -0800

    2022 Day 9 Part 2

--- 2022/day9.nim
+++ 2022/day9.nim
@@ -4,7 +4,9 @@ import util
 type
   Delta    = array[2, int]
   Position = array[2, int]
-  Context  = tuple[head, tail:Position, tailpos:HashSet[Position]]
+  Context[N:static int]  = tuple
+    knots:array[N, Position]
+    tailpos:HashSet[Position]
 
 func ParseDelta(input:string):Delta =
   let (_, dir, dist) = input.scanTuple("$+ $i")
@@ -18,20 +20,24 @@ proc Chase(tail:var Position, head: Position) =
   if 2 in diff.Abs: tail += diff.Clamp(-1, 1)
 
 proc Apply(context:var Context, delta:Delta) =
-  context.tailpos.incl(context.tail)
+  context.tailpos.incl(context.knots[^1])
   # Move one space at a time to capture all the tail locations.
   let times = delta.Abs.max
   let delta = delta.Clamp(-1, 1)
   doTimes times:
-    context.head += delta
-    context.tail.Chase(context.head)
-    context.tailpos.incl(context.tail)
+    context.knots[0] += delta
+    for idx in 1..context.knots.high:
+      context.knots[idx].Chase(context.knots[idx - 1])
+    context.tailpos.incl(context.knots[^1])
 
 proc main =
   const deltas = staticRead("day9.txt").strip.splitLines.map(ParseDelta)
-  var context:Context
-  for delta in deltas: context.Apply(delta)
-  echo("Part1: ", context.tailpos.len)
+  var c2:Context[2]
+  for delta in deltas: c2.Apply(delta)
+  echo("Part1: ", c2.tailpos.len)
+  var c10:Context[10]
+  for delta in deltas: c10.Apply(delta)
+  echo("Part2: ", c10.tailpos.len)
 
 when isMainModule:
   main()
