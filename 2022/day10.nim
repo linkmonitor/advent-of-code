f930aa88a2a3a0a09c2b9d652a16e14ed7c6e40b
Joseph Jaoudi <77069773+jd-yolabs@users.noreply.github.com>
2022-12-12 09:46:15 -0800

    Refactor 2022 Day 10 Part 1

--- 2022/day10.nim
+++ 2022/day10.nim
@@ -1,36 +1,29 @@
+import itertools
 import std/[math, sequtils, strutils]
 
 type
+  CpuState = tuple[pc:int, x:int]
   Instruction = tuple[name:string, value:int]
   Program = seq[Instruction]
-  Cpu = tuple[pc:int, x:int]
 
 func parseInstruction(input:string):Instruction =
   if input.startsWith("addx"): ("addx", input.split[1].parseInt)
   else: ("noop", 0)
 
-iterator execute(cpu: var CPU, program: Program):int =
-  template tick = cpu.pc.inc; yield cpu.pc
-  for instruction in program:
-    if instruction.name == "noop": tick
-    if instruction.name == "addx": tick; tick; cpu.x += instruction.value
+func execute(initial: CpuState, program: Program, upTo:int):seq[CpuState] =
+  var cpuState = initial
+  template tick = cpuState.pc.inc; result.add(cpuState)
+  for instr in program:
+    if instr.name == "noop": tick
+    if instr.name == "addx": tick; tick; cpuState.x += instr.value
+  while cpuState.pc <= upTo: tick
 
-proc signalStrengths(program:Program, limit:int):seq[int] =
-  var cpu:Cpu = (pc:0, x:1)
-  var sampleAt = 20
-  template maybeSample(pc:int) =
-    if pc == sampleAt:
-      result.add(pc * cpu.x)
-      sampleAt += 40
-  for pc in cpu.execute(program):
-    maybeSample(pc)
-  while cpu.pc < limit:
-    cpu.pc.inc
-    maybeSample(cpu.pc)
+func signalStrengths(states:seq[CpuState]):seq[int] =
+  for (pc, x) in islice(states, 20-1, step=40): result.add(pc * x)
 
 proc main =
   const program = staticRead("day10.txt").strip.splitLines.map(parseInstruction)
-  echo("Part1: ", program.signalStrengths(220).sum)
+  echo("Part1: ", (pc:0, x:1).execute(program, upTo=220).signalStrengths.sum)
 
 when isMainModule:
   main()
