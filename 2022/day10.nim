87f22182b014febe57ec5df9550108880f27e9bb
Joseph Jaoudi <77069773+jd-yolabs@users.noreply.github.com>
2022-12-12 17:26:14 -0800

    2022 Day 10 Part 2

--- 2022/day10.nim
+++ 2022/day10.nim
@@ -1,10 +1,12 @@
 import itertools
-import std/[math, sequtils, strutils]
+import std/[math, sequtils, strutils, sugar]
 
 type
   CpuState = tuple[pc:int, x:int]
   Instruction = tuple[name:string, value:int]
   Program = seq[Instruction]
+  Sprite = array[3, int]
+  Screen = array[6, array[40, bool]]
 
 func parseInstruction(input:string):Instruction =
   if input.startsWith("addx"): ("addx", input.split[1].parseInt)
@@ -21,9 +23,24 @@ func execute(initial: CpuState, program: Program, upTo:int):seq[CpuState] =
 func signalStrengths(states:seq[CpuState]):seq[int] =
   for (pc, x) in islice(states, 20-1, step=40): result.add(pc * x)
 
+func toSprite(x:int):Sprite = [x-1, x, x+1]
+
+func `$`(screen:Screen):string =
+  screen.mapIt(it.map(p => ['-', '#'][int(p)]).join).join("\n")
+
+func render(states:seq[CpuState]):Screen =
+  for (pc, x) in states:
+    let row = (pc - 1) div 40
+    let col = (pc - 1) mod 40
+    if col in x.toSprite: result[row][col] = true
+
 proc main =
   const program = staticRead("day10.txt").strip.splitLines.map(parseInstruction)
   echo("Part1: ", (pc:0, x:1).execute(program, upTo=220).signalStrengths.sum)
+
+  let screen:Screen = (pc:0, x:1).execute(program, upTo=200).render
+  echo("Part2: ")
+  echo(screen)
 
 when isMainModule:
   main()
